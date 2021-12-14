3c29a8ff3d6b59436d4faac8f6b46e347ba9e447
Juan Arias <jcamiloat@gmail.com>
2021-12-14 07:05:31 -0500

    Adde Prettier config file

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,64 +6,63 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const SIZES = {
-    small: {
-        '--borderRadius': 4 + 'px',
-        '--height': 8 + 'px',
-    },
-    medium: {
-        '--borderRadius': 4 + 'px',
-        '--height': 12 + 'px',
-    },
-    large: {
-        '--borderRadius': 8 + 'px',
-        '--height': 16 + 'px',
-        '--padding': 4 + 'px',
-    },
+  small: {
+    '--borderRadius': 4 + 'px',
+    '--height': 8 + 'px',
+  },
+  medium: {
+    '--borderRadius': 4 + 'px',
+    '--height': 12 + 'px',
+  },
+  large: {
+    '--borderRadius': 8 + 'px',
+    '--height': 16 + 'px',
+    '--padding': 4 + 'px',
+  },
 };
 
 const ProgressBar = ({ value, size }) => {
-    const styles = SIZES[size];
-    // styles.append('value', value);
-    if (value < 0 || value > 100) {
-        throw new Error('Value must be within 0-100');
-    }
+  const styles = SIZES[size];
+  if (value < 0 || value > 100) {
+    throw new Error('Value must be within 0-100');
+  }
 
-    return (
-        <Wrapper
-            style={styles}
-            role="progressbar"
-            aria-valuenow={value}
-            aria-valuemin="0"
-            aria-valuemax="100"
-        >
-            <OuterBar style={styles} value={value}>
-                <InnerBar style={styles} value={value}></InnerBar>
-            </OuterBar>
-        </Wrapper>
-    );
+  return (
+    <Wrapper
+      style={styles}
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+    >
+      <OuterBar style={styles} value={value}>
+        <InnerBar style={styles} value={value}></InnerBar>
+      </OuterBar>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.div`
-    border-radius: var(--borderRadius);
-    width: 100%;
-    background-color: ${COLORS.transparentGray15};
-    box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
-    padding: var(--padding);
-    overflow: hidden;
+  border-radius: var(--borderRadius);
+  width: 100%;
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  padding: var(--padding);
+  overflow: hidden;
 `;
 
 const OuterBar = styled.div`
-    min-height: var(--height);
-    width: 100%;
-    border-radius: 4px;
-    overflow: hidden;
-    background-color: transparent;
+  min-height: var(--height);
+  width: 100%;
+  border-radius: 4px;
+  overflow: hidden;
+  background-color: transparent;
 `;
 
 const InnerBar = styled.div`
-    min-height: var(--height);
-    width: ${(props) => props.value}%;
-    background-color: ${COLORS.primary};
+  min-height: var(--height);
+  width: ${(props) => props.value}%;
+  background-color: ${COLORS.primary};
 `;
 
 export default ProgressBar;
